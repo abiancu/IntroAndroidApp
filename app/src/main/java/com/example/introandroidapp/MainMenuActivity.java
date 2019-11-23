19fcf19c073be0fdd4caa86c5fcb5d6b652cdfbe
abiancu <alejandrobiancucci@gmail.com>
2019-11-23 12:50:15 -0600

    intent to go from main menu to the weather app

--- app/src/main/java/com/example/introandroidapp/MainMenuActivity.java
+++ app/src/main/java/com/example/introandroidapp/MainMenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.introandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,7 +9,9 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+
 import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -19,16 +22,34 @@ public class MainMenuActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        //LISTENER//
+        Button btnWeather=findViewById(R.id.btnWeather);
+        btnWeather.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                goWeather();
+            }
+        });
+
+
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
     }
 
-    
+    private void goWeather() {
+        Intent intent = new Intent(MainMenuActivity.this, WeatherActivity.class);
+        this.startActivity(intent);
+    }
+
+
 
 }
